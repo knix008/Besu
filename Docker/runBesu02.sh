d25bb7e337320f801a5fb3ae221e697139d68b96
Suho Kwon <suho.kwon@gmail.com>
2023-09-05 14:22:17 +0900

    Add docker run and clean up commands.

---
+++ Docker/runBesu02.sh
@@ -0,0 +1,1 @@
+docker run -p 8546:8546 --mount type=bind,source=/home/shkwon/Projects/Temp/besu/testnode>,target=/var/lib/besu hyperledger/besu:latest --miner-enabled --miner-coinbase fe3b557e8fb62b89f4916b721be55ceb828dbd73 --rpc-ws-enabled --network=dev --data-path=/var/lib/besu
