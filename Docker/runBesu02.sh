6659f253c63bf2cd3f848aa575d1157b2dd6f8e6
Suho Kwon <suho.kwon@gmail.com>
2023-09-05 15:17:39 +0900

    Add docker run and clean up commands.

--- Docker/runBesu02.sh
+++ Docker/runBesu02.sh
@@ -1,1 +1,2 @@
-docker run -p 8546:8546 --mount type=bind,source=/home/shkwon/Projects/Temp/besu/testnode>,target=/var/lib/besu hyperledger/besu:latest --miner-enabled --miner-coinbase fe3b557e8fb62b89f4916b721be55ceb828dbd73 --rpc-ws-enabled --network=dev --data-path=/var/lib/besu
+# Before you run this script, you need to create your "source" directory in your local storage. Then use it for "source".
+docker run -p 8546:8546 --mount type=bind,source=/home/shkwon/Projects/Temp/besu/testnode,target=/var/lib/besu hyperledger/besu:latest --miner-enabled --miner-coinbase fe3b557e8fb62b89f4916b721be55ceb828dbd73 --rpc-ws-enabled --network=dev --data-path=/var/lib/besu
